1f67f98c8803a1572a674fb2818d5a24678a4e86
phexuss <phexuss@gmail.com>
2026-01-17 22:19:42 +0200

    started working on auth

---
+++ prisma/migrations/20260117200446_better_auth_fixes/migration.sql
@@ -0,0 +1,69 @@
+/*
+  Warnings:
+
+  - The primary key for the `Order` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `OrderItem` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `user` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Order" DROP CONSTRAINT "Order_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OrderItem" DROP CONSTRAINT "OrderItem_orderId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Review" DROP CONSTRAINT "Review_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "account" DROP CONSTRAINT "account_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "session" DROP CONSTRAINT "session_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Order" DROP CONSTRAINT "Order_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "userId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Order_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Order_id_seq";
+
+-- AlterTable
+ALTER TABLE "OrderItem" DROP CONSTRAINT "OrderItem_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "orderId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "OrderItem_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "OrderItem_id_seq";
+
+-- AlterTable
+ALTER TABLE "Review" ALTER COLUMN "userId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "account" ALTER COLUMN "userId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "session" ALTER COLUMN "userId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "user" DROP CONSTRAINT "user_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "user_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "user_id_seq";
+
+-- AddForeignKey
+ALTER TABLE "session" ADD CONSTRAINT "session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "account" ADD CONSTRAINT "account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
-import Image from 'next/image';
-import TopBanner from '@/components/layout/top-banner';
+'use client';
+import Link from 'next/link';
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Link
+        href="/sign-up"
+        className="bg-white text-black px-4 py-2 rounded-md hover:bg-gray-200 transition-colors"
+      >
+        Sign Up
+      </Link>
+    </div>
+  );
 }
